ef14555a5a49ca915003fee1500d758cfd2f21e5
davax789 <daffaid567@gmail.com>
2026-01-02 16:48:14 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
